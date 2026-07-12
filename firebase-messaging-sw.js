5eb28677b95da80a26d020a834c889c42f69b7bc
hireactingdrivers-cmyk <hireactingdrivers@gmail.com>
2026-07-12 16:24:15 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBf2I74LYfdifdJnpxy8m-xgwuhwpib3zo",
+  projectId: "spin-ac065",
+  messagingSenderId: "384657177346"
+});
+
+
+const messaging = firebase.messaging();
+
